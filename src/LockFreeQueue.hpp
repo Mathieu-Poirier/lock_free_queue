e82c5ca04fd2ca20ab11ce6f05bf46e213ece2ed
Mathieu Poirier <mathieuworkemail@gmail.com>
2026-04-01 22:22:26 -0400

    Starting to create API for LockFreeQueue.hpp.

--- src/LockFreeQueue.hpp
+++ src/LockFreeQueue.hpp
@@ -9,4 +9,9 @@
 
 struct LockFreeQueue;
 
+// @ For C-style opaque we usually just use pointers
+// @ Type should maybe just be uint64_t
 LockFreeQueue* CreateQueue(const LockFreeQueueBuilder* builder);
+LockFreeQueue* DestroyQueue(LockFreeQueue queue);
+LockFreeQueue* AppendTo(LockFreeQueue* queue, uint64_t value);
+
