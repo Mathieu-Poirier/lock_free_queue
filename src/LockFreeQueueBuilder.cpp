c41b6e44c8d516d97c104dd595213959da53bfda
Mathieu Poirier <mathieuworkemail@gmail.com>
2026-04-04 01:44:27 -0400

    Implementing filePath tests and runtime checks.

--- src/LockFreeQueueBuilder.cpp
+++ src/LockFreeQueueBuilder.cpp
@@ -5,10 +5,12 @@
 
 // @ Parse this from CLI
 constexpr unsigned int MAX_QUEUE_SIZE = 1024;
+constexpr unsigned int MAX_FILEPATH_LENGTH = 4096;
 
 struct LockFreeQueueBuilder {
     std::size_t size;
     const char* filePath;
+    size_t filePathLength;
 };
 
 void LockFreeQueueBuilderInitialize(LockFreeQueueBuilder* lockFreeQueueBuilder){
@@ -37,6 +39,25 @@ void BuildSize(LockFreeQueueBuilder* lockFreeQueueBuilder, std::size_t size){
     lockFreeQueueBuilder->size = size;
 }
 
-void BuildFilePath(LockFreeQueueBuilder* lockFreeQueueBuilder, const char* filePath, const std::size_t filePathLenght){
-    
+void BuildFilePath(LockFreeQueueBuilder* lockFreeQueueBuilder, const char* filePath, const std::size_t filePathLength){
+    // Probably make some guards on input too
+    if(filePath == nullptr){
+        throw std::invalid_argument("You cannot make a LockFreeQueueBuilder with filePath == nullptr");
+    }
+    if(filePathLength <= 0){
+        throw std::invalid_argument("You cannot make a LockFreeQueueBuilder with filePathLength <= 0");
+    }
+    // Need to enforce extra things in an encapsulated function on the caller side to minimize misuse where buffer can be larger than size which truncates
+    if(filePathLength > MAX_FILEPATH_LENGTH){
+        throw std::invalid_argument("You cannot make a LockFreeQueueBuilder with filePathLength > MAX_FILEPATH_LENGTH");
+    }
+    // @ Length to index, this is UB though so might still pass even when the string isn't null terminated presumably
+    if(filePath[filePathLength - 1] != '\0'){
+        throw std::invalid_argument("You cannot make a LockFreeQueueBuilder with something that is not null terminated");
+    }
+    // 
+    // Set
+    lockFreeQueueBuilder->filePath = filePath;
+    // Max we will read from the buffer
+    lockFreeQueueBuilder->filePathLength = filePathLength;
 }
--- tests/test_LockFreeQueueBuilder.cpp
+++ tests/test_LockFreeQueueBuilder.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include "LockFreeQueueBuilder.hpp"
 
+// Size
 TEST(QueueBuilderTest, CreateQueueBuilderWithZeroSize){
     LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
     EXPECT_THROW(BuildSize(queueBuilder, 0uz), std::invalid_argument);
@@ -14,6 +15,7 @@ TEST(QueueBuilderTest, CreateQueueBuilderWithSizeBiggerThanMaxSize){
     DestroyLockFreeQueueBuilder(queueBuilder);
 }
 
+// Filepath
 TEST(QueueBuilderTest, PassInNullPtrToQueueBuilderBuildFilePath){
     LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
     EXPECT_THROW(BuildFilePath(queueBuilder, nullptr, 0uz), std::invalid_argument);
@@ -34,6 +36,7 @@ TEST(QueueBuilderTest, NoNullTerminatorQueueBuilderBuildFilePath){
     DestroyLockFreeQueueBuilder(queueBuilder);
 }
 
+// This one is a little weird too, we should add some UB checks
 TEST(QueueBuilderTest, PathLength4097AndArbitraryBufferQueueBuilderBuildFilePath){
     LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
     char buf[] = {'a', 'b', 'c'};
@@ -50,12 +53,51 @@ TEST(QueueBuilderTest, PathOverMaxLengthAndArbitraryBufferQueueBuilderBuildFileP
     DestroyLockFreeQueueBuilder(queueBuilder);
 }
 
-TEST(QueueBuilderTest, PathZeroSizeAndArbitraryBufferQueueBuilderBuildFilePath){
+TEST(QueueBuilderTest, PathZeroLengthAndArbitraryBufferQueueBuilderBuildFilePath){
     LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
     char buf[] = {'a', 'b', 'c'};
     const char* p = buf;
     EXPECT_THROW(BuildFilePath(queueBuilder, buf, 0uz), std::invalid_argument);
     DestroyLockFreeQueueBuilder(queueBuilder);
 }
+
+TEST(QueueBuilderTest, PathLengthDoesNotMatchBufferLengthSmaller_QueueBuilderBuildFilePath){
+    LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
+    const char* str_buf = "Hello";
+    EXPECT_THROW(BuildFilePath(queueBuilder, str_buf, 5uz), std::invalid_argument);
+    DestroyLockFreeQueueBuilder(queueBuilder);
+}
+
+
+TEST(QueueBuilderTest, PathLengthDoesNotMatchBufferLengthLarger_QueueBuilderBuildFilePath){
+    LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
+    const char* str_buf = "Hello";
+    EXPECT_THROW(BuildFilePath(queueBuilder, str_buf, 7uz), std::invalid_argument);
+    DestroyLockFreeQueueBuilder(queueBuilder);
+}
+
+TEST(QueueBuilderTest, PathLengthParameterIsOverLimit_QueueBuilderBuildFilePath){
+    LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
+    const char* str_buf = "Hello";
+    EXPECT_THROW(BuildFilePath(queueBuilder, str_buf, 4097), std::invalid_argument);
+    DestroyLockFreeQueueBuilder(queueBuilder);
+}
+
+TEST(QueueBuilderTest, RealPathDoesNotThrow_QueueBuilderBuildFilePath){
+    LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
+    EXPECT_NO_THROW(BuildFilePath(queueBuilder, "AAAAAAA", 8));
+    DestroyLockFreeQueueBuilder(queueBuilder);
+}
+
+// TEST(QueueBuilderTest, PathBufferLenghtIsOverLimit_QueueBuilderBuildFilePath){
+//     LockFreeQueueBuilder* queueBuilder = CreateLockFreeQueueBuilder();
+//     char str_buf[4098]{};
+//     for(auto i{0uz}; i < 4097; i++){
+//         str_buf[i] = 'A';
+//     }
+//     EXPECT_THROW(BuildFilePath(queueBuilder, str_buf, 4096), std::invalid_argument);
+//     DestroyLockFreeQueueBuilder(queueBuilder);
+// }
+
 
 // @ Maybe santinize bytes like /n /t
